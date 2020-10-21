bc37040d4443a419a017f11cc7e3c1075bd251fe
Alexey Antonov <alexeyantonov@me.com>
2020-10-21 16:57:39 +0200

    Initial Commit

---
+++ OrientaleNapp/OrientaleNappApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientaleNappApp.swift
+//  OrientaleNapp
+//
+//  Created by Alexey Antonov on 21/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct OrientaleNappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
